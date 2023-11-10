b24c7bd898b36559202125ab8fc25ba8f92c7e4d
codemmk <2107228359@qq.com>
2023-11-11 06:42:11 +0800

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="欢迎使用 Typecho 主题开发脚手架" />
   </div>
 </template>
 
