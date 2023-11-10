b0c08cb0737e54bf8f54cf29e6edcea79dfbfae3
codemmk <2107228359@qq.com>
2023-11-11 07:24:19 +0800

    add rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,15 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
+    'prettier'
   ],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off'
   }
 }
